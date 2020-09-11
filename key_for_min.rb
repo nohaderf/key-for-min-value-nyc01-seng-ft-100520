e55c74e4922af151ca4f49b88c822c492ba712cd
Freda Hon <freda.hon@gmail.com>
2020-09-11 14:38:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash.each do |item, price|
+    min_value = name_hash[price]
+    if price < min_value
+        min_value = price
+    end
+  end
+  min_value
 end
